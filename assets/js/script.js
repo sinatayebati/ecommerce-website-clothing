1dc01dc0e2c9743b2bfa5cbe8cedc9e1f94bba7e
sinatayebati <sinatayebati@gmail.com>
2023-07-15 19:19:30 -0500

    swiper initiated

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,21 @@ const header = document.querySelector("[data-header]")
 window.addEventListener("scroll", function() {
     header.classList[this.scrollY > 50 ? "add" : "remove"]("active");
 });
+
+/**
+ * SWIPER   
+ */
+
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    lopp: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
