2557f2f27a0861ae294495fc78559de441b115e0
sinatayebati <sinatayebati@gmail.com>
2023-07-18 00:15:27 -0500

    swiper updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,12 +26,16 @@ window.addEventListener("scroll", function() {
  * SWIPER   
  */
 
-let swiper = new Swiper(".new__container", {
-  cssMode: true,
-  lopp: true,
-  
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    lopp: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
 });
